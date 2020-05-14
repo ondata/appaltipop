4de48019d5c1bbce91c300490aebaac85aa21f4c
Alessio Cimarelli <jenkin@dataninja.it>
2020-05-14 10:35:26 +0200

    Add support to multi file processing

--- jsonl/main.py
+++ jsonl/main.py
@@ -11,10 +11,22 @@ if __name__ == "__main__":
         exit(1)
 
     json_path = Path(sys.argv[1])
-    jsonl_path = Path("./", json_path.name).with_suffix(".jsonl")
 
-    with open(json_path) as f:
-        l = json.load(f)
-
-    with jsonlines.open(jsonl_path, mode='w') as writer:
-        writer.write_all(l)
+    if json_path.is_dir():
+        for json_file in json_path.glob("*.json"):
+            if json_file.is_file():
+                jsonl_file = Path("./", json_file.name).with_suffix(".jsonl")
+                with open(json_file) as f:
+                    l = json.load(f)
+                with jsonlines.open(jsonl_file, mode='w') as writer:
+                    writer.write_all(l)
+            else:
+                logging.warning("Failed to open {}, skip".format(json_file))
+    elif json_path.is_file():
+        jsonl_file = Path("./", json_path.name).with_suffix(".jsonl")
+        with open(json_path) as f:
+            l = json.load(f)
+        with jsonlines.open(jsonl_file, mode='w') as writer:
+            writer.write_all(l)
+    else:
+        logging.error("Failed to open {}".format(json_path))
